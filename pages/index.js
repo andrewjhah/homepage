dcfa5bfacb9d956f652a1fedcd693d7da24cfed6
Andrew Hah <andrew.hah@gmail.com>
2023-09-08 19:51:51 +0900

    index.js

--- pages/index.js
+++ pages/index.js
@@ -28,24 +28,13 @@ const ProfileImage = chakra(Image, {
 
 const Home = () => (
   <Layout>
-    <Container>
-      <Box
-        borderRadius="lg"
-        mb={6}
-        p={3}
-        textAlign="center"
-        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-        css={{ backdropFilter: 'blur(10px)' }}
-      >
-        Hello, I&apos;m an indie app developer based in Japan!
-      </Box>
-
+    <Container maxW = "container.md">
       <Box display={{ md: 'flex' }}>
         <Box flexGrow={1}>
           <Heading as="h2" variant="page-title">
             Andrew Hah
           </Heading>
-          <p>Digital Craftsman ( Artist / Developer / Designer )</p>
+          <p>Math @ UChicago</p>
         </Box>
         <Box
           flexShrink={0}
@@ -57,18 +46,18 @@ const Home = () => (
             borderColor="whiteAlpha.800"
             borderWidth={2}
             borderStyle="solid"
-            w="100px"
-            h="100px"
+            w="130px"
+            h="130px"
             display="inline-block"
             borderRadius="full"
             overflow="hidden"
           >
             <ProfileImage
-              src="/images/takuya.jpg"
+              src="/images/andrew.jpg"
               alt="Profile image"
               borderRadius="full"
-              width="100"
-              height="100"
+              width="170"
+              height="170"
             />
           </Box>
         </Box>
