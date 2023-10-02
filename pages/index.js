6d77bea16c892828fc67ae8ee733652289373d4a
Andrew Hah <andrew.hah@gmail.com>
2023-10-02 12:34:15 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ const Home = () => (
           <Heading as="h2" variant="page-title">
             Andrew Hah
           </Heading>
-          <p>Math + CS @ UChicago</p>
+          <p>Mathematics @ UChicago</p>
           <Stack mt={4} spacing={4} direction='row' align='center'>
             <Link href="/files/resume.pdf" target="_blank">
               <Button rightIcon={<ChevronRightIcon />} colorScheme="red" size='md' variant='solid'>
