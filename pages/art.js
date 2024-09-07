a34a9587c6bd9a07272f0026d581b00e3e7c9c40
Andrew Hah <andrew.hah@gmail.com>
2024-09-06 22:45:34 -0500

    art

--- pages/art.js
+++ pages/art.js
@@ -4,6 +4,13 @@ import Section from '../components/section'
 import { GridItem } from '../components/grid-item'
 
 import thumbRubbing1 from '../public/images/art/rubbing-1.png'
+import thumbRubbing2 from '../public/images/art/rubbing-2.png'
+import thumbRubbing3 from '../public/images/art/rubbing-3.png'
+import thumbCharc1 from '../public/images/art/charc-1.png'
+import thumbCharc2 from '../public/images/art/charc-2.png'
+import thumbCenti1 from '../public/images/art/centi-1.png'
+import thumbCenti2 from '../public/images/art/centi-2.png'
+import thumbPhoto1 from '../public/images/art/photo-1.png'
 
 const Projects = () => (
   <Layout title="Projects">
@@ -14,14 +21,63 @@ const Projects = () => (
         </Heading>
       </Stack>
 
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
+      <SimpleGrid columns={[1, 1, 3]} gap={6}>
         <Section>
         <GridItem
             href="/images/art/rubbing-1.png" 
             target="_blank" 
             thumbnail={thumbRubbing1}
           >
-
+          </GridItem>
+          <GridItem
+            href="/images/art/rubbing-2.png" 
+            target="_blank" 
+            thumbnail={thumbRubbing2}
+          >
+          </GridItem>
+          <GridItem
+            href="/images/art/rubbing-3.png" 
+            target="_blank" 
+            thumbnail={thumbRubbing3}
+          >
+          </GridItem>
+        </Section>
+      </SimpleGrid>
+      <SimpleGrid columns={[1, 1, 2]} gap={6}>
+        <Section>
+        <GridItem
+            href="/images/art/charc-1.png" 
+            target="_blank" 
+            thumbnail={thumbCharc1}
+          >
+          </GridItem>
+          <GridItem
+            href="/images/art/charc-2.png" 
+            target="_blank" 
+            thumbnail={thumbCharc2}
+          >
+          </GridItem>
+          <GridItem
+            href="/images/art/centi-1.png" 
+            target="_blank" 
+            thumbnail={thumbCenti1}
+          >
+          </GridItem>
+          <GridItem
+            href="/images/art/centi-2.png" 
+            target="_blank" 
+            thumbnail={thumbCenti2}
+          >
+          </GridItem>
+        </Section>
+      </SimpleGrid>
+      <SimpleGrid columns={[1, 1, 4]} gap={6}>
+        <Section>
+        <GridItem
+            href="/images/art/photo-1.png" 
+            target="_blank" 
+            thumbnail={thumbPhoto1}
+          >
           </GridItem>
         </Section>
       </SimpleGrid>
